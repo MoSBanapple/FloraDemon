51ce6bad3c7b89279f2041858b0046cd8ab3d925
MoSBanapple <derekz1234@gmail.com>
2023-09-29 20:26:52 -0500

    Initial Commit

--- src/main/java/florademon/cards/CultivateLily.java
+++ src/main/java/florademon/cards/CultivateLily.java
@@ -44,6 +44,7 @@ public class CultivateLily extends BaseCard {
         if (this.upgraded){
             addToBot(new BladedLilyAction(newLily));
         }
+
         addToBot(new BloomAction());
     }
 }
